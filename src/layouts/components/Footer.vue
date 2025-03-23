877205588fd58a6eb08cc40ac367b6f51f487b57
Niks890 <minhtrivo802@gmail.com>
2025-03-23 09:32:48 +0700

    ready_to_project

---
+++ src/layouts/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <hr>
+  <div class="h-100 d-flex flex-wrap align-center justify-space-between">
+    <span class="d-flex align-center">
+      &copy;
+      {{ new Date().getFullYear() }}
+     Made By <a
+        href="javascript:void(0)"
+        rel="noopener noreferrer"
+        class="text-primary ms-1"
+      >Me!</a>
+    </span>
+    <span class="d-flex align-center">
+      Developed By <a
+        href="javascript:void(0)"
+        rel="noopener noreferrer"
+        class="text-primary ms-1"
+      >Me!</a>
+    </span>
+  </div>
+</template>
